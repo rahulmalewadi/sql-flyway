4a75f09957879b09bd1380811b16e4c6b56e842c
beluga_777 <82512132+rahulmalewadi@users.noreply.github.com>
2021-10-07 16:04:01 +0530

    Update 1.0.0__Create_users_table.sql

--- sql/1.0.0__Create_users_table.sql
+++ sql/1.0.0__Create_users_table.sql
@@ -1,36 +1,9 @@
-/* Single line comment */
-CREATE TABLE test_data (
- value VARCHAR(25) NOT NULL PRIMARY KEY
+CREATE TABLE employees (
+   employee_id   NUMERIC       NOT NULL,
+   first_name    VARCHAR(1000) NOT NULL,
+   last_name     VARCHAR(1000) NOT NULL,
+   date_of_birth DATE                  ,
+   phone_number  VARCHAR(1000) NOT NULL,
+   junk          CHAR(1000)            ,
+   CONSTRAINT employees_pk PRIMARY KEY (employee_id)
 );
-
-
-/*
-Multi-line
-comment
-*/
-
--- Multi-statement PostgreSQL function
-CREATE FUNCTION AddData() RETURNS INTEGER
-AS $$
-   BEGIN
-    INSERT INTO test_data (value) VALUES ('Hello');
-    RETURN 1;
-  END;
-$$ LANGUAGE plpgsql;
-
-SELECT *  INTO TEMP adddata_temp_table FROM AddData() ;
-
--- Single-statement PostgreSQL function
-CREATE FUNCTION add(integer, integer) RETURNS integer
-   LANGUAGE sql IMMUTABLE STRICT
-   AS $_$select $1 + $2;$_$;
-
--- Placeholder
-INSERT INTO ${tableName} (name) VALUES ('Mr. T');
-
--- COPY ... FROM STDIN
-COPY copy_test (c1, c2, c3) FROM stdin;
-1	utf8: ümlaute: äüß	NaN
-2	\N	123
-3	text	123.234444444444449
-\.
